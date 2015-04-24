092c5a5c3702646860475cdc1f413934f586400b
takagi <anct.e0721@gmail.com>
2015-04-24 15:19:13 +0900

    change pass

--- config.php
+++ config.php
@@ -5,18 +5,18 @@
 //////////////////////////////////////////////////
 
 // 本体のディレクトリパス
-define("DIR","/home/users/2/secret.jp-ancte0721/web/dm/");
+define("DIR","/home/users/test.jp/web/dm/");
 
 // DBホスト
-define("DB_HOST","mysql565.phy.lolipop.jp");
+define("DB_HOST","mysql.phy.test.jp");
 
 // DB名
-define("DB_NAME","LAA0181923-life");
+define("DB_NAME","LAA01-dm");
 
 // DBのID
-define("DB_ID","LAA0181923");
+define("DB_ID","LAA01");
 
 // DBのPASS
-define("DB_PASS","nonono");
+define("DB_PASS","000000");
 
 ?>
